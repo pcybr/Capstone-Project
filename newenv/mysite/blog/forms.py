aeb8f314961cd379d2bde39de613f2100a111e12
Nicholas Tuzinkiewicz <nicholastuzinkiewicz@d-172-27-212-35.dhcp.virginia.edu>
2018-09-16 14:00:04 -0400

    updates

---
+++ newenv/mysite/blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+from .models import PostModel
+
+
+class PostModelForm(forms.ModelForm):
+    class Meta:
+        model = PostModel
+        fields = [
+            'title',
+            'content'
+        ]
+
