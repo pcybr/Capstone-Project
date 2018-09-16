b862556e04b1c1c11750d6e14cd4dab8da2b6e59
Nicholas Tuzinkiewicz <nicholastuzinkiewicz@d-172-27-212-35.dhcp.virginia.edu>
2018-09-16 14:37:11 -0400

    change

--- newenv/mysite/mysite/urls.py
+++ newenv/mysite/mysite/urls.py
@@ -22,5 +22,6 @@ from blog.views import post_model_list_view
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls'))
+    #url(r'^blog/', include('blog.urls'))
+    url(r'^blog/',include(('blog.urls','blog'),namespace='blog')),
 ]
