78b16968c9596968648ec37ce7dee46b402dc5b6
pcybr <pic5fe@virginia.edu>
2018-09-16 14:25:02 -0400

    IDIOT

--- newenv/mysite/blog/views.py
+++ newenv/mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import PostModel
+from .forms import PostModelForm
 
 def post_model_list_view(request):
     qs = PostModel.objects.all()
@@ -8,6 +9,21 @@ def post_model_list_view(request):
 	context = {
 		"object_list": qs,
 	}
+    
 	return render(request, template, context)
+
+def post_model_create_view(request):
+    template = "blog/create-view.html"
+    form = PostModelForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        context = {
+            "form": PostModelForm()
+        }
+    return render(request, template, context)
 
 # Create your views here.
