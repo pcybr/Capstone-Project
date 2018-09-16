5dedd718e265e16dd0be2eedbb608af159319654
Nicholas Tuzinkiewicz <nicholastuzinkiewicz@d-172-27-212-35.dhcp.virginia.edu>
2018-09-16 14:32:30 -0400

    moves

--- newenv/mysite/blog/urls.py
+++ newenv/mysite/blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import post_model_list_view, post_model_create_view
+from .views import post_model_list_view, post_model_create_view, post_model_detail_view
 
 urlpatterns = [
 	url(r'^$', post_model_list_view, name='list'),
 	url(r'^create/$', post_model_create_view, name='create'),
+	url(r'^(?P<id>\d+)/$', post_model_detail_view, name='detail'),
 ]
--- newenv/mysite/blog/views.py
+++ newenv/mysite/blog/views.py
@@ -1,29 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import PostModel
-from .forms import PostModelForm
+from django.shortcuts import get_object_or_404
+
 
 def post_model_list_view(request):
     qs = PostModel.objects.all()
     template = "blog/list-view.html"
-	context = {
+    context = {
 		"object_list": qs,
 	}
-    
 	return render(request, template, context)
 
-def post_model_create_view(request):
-    template = "blog/create-view.html"
-    form = PostModelForm(request.POST or None)
+def post_model_detail_view(request, id=None):
+    obj = get_object_or_404(PostModel, id=id)
     context = {
-        "form": form
+        "object": obj,
     }
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.save()
-        context = {
-            "form": PostModelForm()
-        }
+    template = "blog/detail-view.html"
     return render(request, template, context)
 
 # Create your views here.
