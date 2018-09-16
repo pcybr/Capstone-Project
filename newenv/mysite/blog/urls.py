41057ac1679f57271bf27725c5782082f311173f
Nicholas Tuzinkiewicz <nicholastuzinkiewicz@d-172-27-212-35.dhcp.virginia.edu>
2018-09-16 14:22:22 -0400

    test

--- newenv/mysite/blog/urls.py
+++ newenv/mysite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import post_model_list_view
+from .views import post_model_list_view, post_model_create_view
 
 urlpatterns = [
 	url(r'^$', post_model_list_view, name='list'),
+	url(r'^create/$', post_model_create_view, name='create'),
 ]
